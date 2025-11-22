5973413d3833775a3454e257bb50feda65d5d039
Mohit Verma <vermam.work@gmail.com>
2025-11-22 08:05:53 +0530

    Update generate_order_items.sql

--- data_generation/generate_order_items.sql
+++ data_generation/generate_order_items.sql
@@ -1,8 +1,17 @@
-INSERT INTO `ecommerce-analytics-12345.raw_data.order_items_raw`
-WITH items AS (
-SELECT o.order_id,p.product_id,CEIL(RAND()*3),
-p.base_price + RAND()*20
-FROM `ecommerce-analytics-12345.raw_data.orders_raw` o
-JOIN `ecommerce-analytics-12345.raw_data.products_raw` p ON TRUE
-WHERE RAND()<0.25)
-SELECT * FROM items;
+CREATE OR REPLACE TABLE `ecommerce-analytics-12345.raw_data.order_items_raw` AS
+WITH orders AS (
+  SELECT order_id FROM `ecommerce-analytics-12345.raw_data.orders_raw`
+),
+products AS (
+  SELECT product_id, price FROM `ecommerce-analytics-12345.raw_data.products_raw`
+)
+SELECT
+    GENERATE_UUID() AS order_item_id,
+    o.order_id,
+    p.product_id,
+    CAST(1 + FLOOR(RAND() * 3) AS INT64) AS quantity,
+    p.price AS price
+FROM orders o
+JOIN products p
+ON RAND() < 0.02   -- randomly assign products to orders (~3 items/order)
+LIMIT 60000;
