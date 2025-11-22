90bdfc0af378c5be9555abf6880d2b135fa24ab9
Mohit Verma <vermam.work@gmail.com>
2025-11-22 08:05:26 +0530

    Update generate_orders.sql

--- data_generation/generate_orders.sql
+++ data_generation/generate_orders.sql
@@ -1,6 +1,11 @@
-INSERT INTO `ecommerce-analytics-12345.raw_data.orders_raw`
-WITH cust AS (SELECT customer_id FROM `ecommerce-analytics-12345.raw_data.customers_raw`)
-SELECT CONCAT("O",GENERATE_UUID()), (SELECT customer_id FROM cust ORDER BY RAND() LIMIT 1),
-DATE_SUB(CURRENT_DATE(), INTERVAL CAST(FLOOR(RAND()*180) AS INT64) DAY),
-IF(RAND()>0.05,"Completed","Cancelled")
-FROM UNNEST(GENERATE_ARRAY(1,10000));
+CREATE OR REPLACE TABLE `ecommerce-analytics-12345.raw_data.orders_raw` AS
+WITH cust AS (
+  SELECT customer_id FROM `ecommerce-analytics-12345.raw_data.customers_raw`
+)
+SELECT
+    GENERATE_UUID() AS order_id,
+    customer_id,
+    DATE_SUB(CURRENT_DATE(), INTERVAL CAST(FLOOR(RAND() * 365) AS INT64) DAY) AS order_date,
+    ROUND(20 + RAND() * 400, 2) AS total_amount
+FROM cust, UNNEST(GENERATE_ARRAY(1, 4)) AS x   -- ~4 orders per customer = 20k orders
+LIMIT 20000;
