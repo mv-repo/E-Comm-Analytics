4775f3023b9100b10339cea088abe4ef96f98291
Mohit Verma <vermam.work@gmail.com>
2025-11-22 08:04:11 +0530

    Update generate_customers.sql

--- data_generation/generate_customers.sql
+++ data_generation/generate_customers.sql
@@ -1,6 +1,10 @@
-INSERT INTO `ecommerce-analytics-12345.raw_data.customers_raw`
-SELECT CONCAT("C", GENERATE_UUID()), INITCAP(SPLIT(name," ")[OFFSET(0)]),
-INITCAP(SPLIT(name," ")[OFFSET(1)]), CONCAT(REPLACE(LOWER(name)," ","."),"@example.com"),
-DATE_SUB(CURRENT_DATE(), INTERVAL CAST(FLOOR(RAND()*365) AS INT64) DAY)
-FROM UNNEST(GENERATE_ARRAY(1,1000)),UNNEST(["john doe","emma stone","li wei",
-"fatima ali","alex hunter","sara kim","michael smith"]);
+CREATE OR REPLACE TABLE `ecommerce-analytics-12345.raw_data.customers_raw` AS
+WITH countries AS (
+    SELECT ['USA','Canada','UK','Germany','France','Australia','India','Brazil','Japan','UAE'] AS c
+)
+SELECT
+    GENERATE_UUID() AS customer_id,
+    CONCAT('Customer ', CAST(ROW_NUMBER() OVER() AS STRING)) AS customer_name,
+    c[OFFSET(CAST(FLOOR(RAND() * ARRAY_LENGTH(c)) AS INT64))] AS country,
+    DATE_SUB(CURRENT_DATE(), INTERVAL CAST(FLOOR(RAND()*900) AS INT64) DAY) AS signup_date
+FROM countries, UNNEST(GENERATE_ARRAY(1, 5000)) AS x;
